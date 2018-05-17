986842de80b8c674cd130ad9154ac4475975c630
CX2016CK <1520577322@qq.com>
2018-05-17 12:43:28 +0800

    第二次上传 带分支2018年5月17日12:43:18

---
+++ EasyBuy_Wh/src/cn/wh/shop/s.java
@@ -0,0 +1,10 @@
+
+package cn.wh.shop;
+/**
+ * @2018年5月16日
+ * @王 鸿
+ * @Administrator
+ */
+public class s {
+
+}
---
+++ EasyBuy_Wh/src/cn/wh/shop/util/FirstFilter.java
@@ -0,0 +1,41 @@
+package cn.wh.shop.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * @2018年5月10日
+ * @王 鸿
+ * @Administrator
+ */
+@WebFilter(filterName = "FirstFilter", urlPatterns = "/*")
+public class FirstFilter implements Filter {
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+
+	}
+
+}
